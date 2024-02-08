4fad4a8994cd5ea5d1c17c98291bd06afc9d5428
4ekhov <77397712+4ekhov@users.noreply.github.com>
2024-02-08 20:24:49 +0300

    logs update

--- simple_parser.cpp
+++ simple_parser.cpp
@@ -5,22 +5,93 @@
 #include <windows.h>
 #include <bitset>
 
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+std::ofstream LogFile;
+
+// выводит в консоль какая секция была открыта
+void PrintLogSectOpen(bool is_scheme_section, bool is_section_in_main) {
+    SetConsoleTextAttribute(hConsole, 2);
+
+    if (is_scheme_section) {
+        std::cout << '\n' << "MAIN SECTION OPENED ";
+        LogFile << '\n' << '\n' << "MAIN SECTION OPENED ";
+    } else if (is_section_in_main) {
+        SetConsoleTextAttribute(hConsole, 7);
+        std::cout << '\n' << "----";
+        LogFile << '\n' << "----";
+        SetConsoleTextAttribute(hConsole, 2);
+        std::cout << " INTER SECTION OPENED ";
+        LogFile << " INTER SECTION OPENED ";
+    } else {
+        std::cout << '\n' << "SECTION OPENED ";
+        LogFile << '\n' << "SECTION OPENED ";
+    }
+}
+
+// выводит в консоль имя секции
+void PrintSectName(char* buffer, bool is_section_in_main) {
+    SetConsoleTextAttribute(hConsole, 12);
+    std::cout << "section name: ";
+    LogFile << "section name: ";
+
+    SetConsoleTextAttribute(hConsole, 1);
+    for (uint32_t i = 0; i < 4; ++i) {
+        std::cout << buffer[i];
+        LogFile << buffer[i];
+    }
+
+    SetConsoleTextAttribute(hConsole, 12);
+    if (!is_section_in_main) {
+        std::cout << '\n' << "section data: ";
+        LogFile << '\n' << "section data: ";
+    } else {
+        SetConsoleTextAttribute(hConsole, 7);
+        std::cout << '\n' << "----";
+        LogFile << '\n' << "----";
+        SetConsoleTextAttribute(hConsole, 12);
+        std::cout << " section data: ";
+        LogFile << " section data: ";
+    }
+}
+
+// выводит в консоль размер секции
+void PrintSectSize(const uint32_t& section_size) {
+    SetConsoleTextAttribute(hConsole, 12);
+    std::cout << "section size: ";
+    LogFile << "section size: ";
+
+    SetConsoleTextAttribute(hConsole, 1);
+    std::cout << section_size << ' ';
+    LogFile << section_size << ' ';
+}
+
+// выводит в консоль размер файла
+void PrintFileSize(const uint32_t& file_size) {
+    SetConsoleTextAttribute(hConsole, 12);
+    std::cout << '\n' << "file size: ";
+    LogFile << '\n' << "file size: ";
+    SetConsoleTextAttribute(hConsole, 1);
+    std::cout << file_size << " bytes \n";
+    LogFile << file_size << " bytes \n";
+}
+
+
 int main() {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
     // setlocale(LC_ALL, "rus");
 
-    std::ifstream file;
+    std::ifstream SchemeFile;
 
-    file.open(L"Новая схема.схема", std::ios_base::binary);
+    SchemeFile.open(L"Линия.схема", std::ios_base::binary);
+    LogFile.open("logs\\SchemeLogs.txt", std::ios_base::binary);
 
-    if (!file) {
+    if (!SchemeFile or !SchemeFile) {
         std::cout << "File wasn't open\n";
     }
 
-    file.seekg(0, std::ios::end);
-    uint32_t file_size = file.tellg();
-    file.seekg(0, std::ios::beg);
+    SchemeFile.seekg(0, std::ios::end);
+    uint32_t file_size = SchemeFile.tellg();
+    SchemeFile.seekg(0, std::ios::beg);
 
     std::string section_names[10] = {"ARM ", "lib", "schm",
                                      "cach", "link", "sect",
@@ -50,89 +121,72 @@ int main() {
 
     std::string check_name;
 
-    std::bitset<8> print_binary;
+    std::bitset<8> print_byte;
+    std::bitset<32> print_four_bytes;
 
-    bool is_scheme_section = true;
+    bool is_main_section = true;
     bool is_section_in_main = false;
+
+    bool is_in_setion = true;
 
     uint32_t main_section_size;
 
-    while (!file.eof() and file.get(byte)) {
+    // выводит в консоль размер файла
+    PrintFileSize(file_size);
+
+    while (!SchemeFile.eof() and SchemeFile.get(byte)) {
 
         if (!section_opened && byte == section_flag) {
             section_opened = true;
 
-            uint32_t curr_pos = file.tellg();
-            if (is_section_in_main && (curr_pos == main_section_size - 4)) {
+            uint32_t curr_pos = SchemeFile.tellg();
+            if (is_section_in_main && (curr_pos > main_section_size)) {
                 is_section_in_main = false;
             }
 
-            SetConsoleTextAttribute(hConsole, 2);
-
-            if (is_scheme_section)
-                std::cout << '\n' << "MAIN SECTION OPENED ";
-            else if (is_section_in_main) {
-                SetConsoleTextAttribute(hConsole, 7);
-                std::cout << '\n' << "----";
-                SetConsoleTextAttribute(hConsole, 2);
-                std::cout << " INTER SECTION OPENED ";
-            } else
-                std::cout << '\n' << "SECTION OPENED ";
+            // выводит в консоль какая секция была открыта
+            PrintLogSectOpen(is_main_section, is_section_in_main);
 
             if (buffer != nullptr) {
                 delete[] buffer;
             }
             buffer = new char[4];
 
-            file.read(buffer, 4);
-            for (uint8_t i = 0; i < 4; ++i) {
-                if (buffer[i] != 0) {
-                    section_size = (section_size << 8) | (static_cast<uint8_t>(buffer[i]));
-                }
+            SchemeFile.read(buffer, 4);
+
+            section_size = 0;
+            for (int8_t i = 3; i >= 0; --i) {
+                section_size |= static_cast<uint8_t>(buffer[i]);
+
+                if (i != 0)
+                    section_size <<= 8;
             }
 
-            SetConsoleTextAttribute(hConsole, 12);
-            std::cout << "section size: ";
-
-            SetConsoleTextAttribute(hConsole, 1);
-            std::cout << section_size << ' ';
-
-            SetConsoleTextAttribute(hConsole, 12);
-            std::cout << "section name: ";
+            // выводит в консоль размер секции
+            PrintSectSize(section_size);
 
             delete[] buffer;
             buffer = new char[4];
 
-            file.read(buffer, 4);
+            SchemeFile.read(buffer, 4);
 
-            SetConsoleTextAttribute(hConsole, 1);
-            for (uint32_t i = 0; i < 4; ++i) {
-                std::cout << buffer[i];
-            }
+            // выводит в консоль имя секции
+            PrintSectName(buffer, is_section_in_main);
 
-            SetConsoleTextAttribute(hConsole, 12);
-            if (!is_section_in_main)
-                std::cout << '\n' << "section data: ";
-            else {
-                SetConsoleTextAttribute(hConsole, 7);
-                std::cout << '\n' << "----";
-                SetConsoleTextAttribute(hConsole, 12);
-                std::cout << " section data: ";
-            }
-
-            if (!is_scheme_section) {
+            if (!is_main_section) {
                 delete[] buffer;
                 buffer = new char[section_size];
 
-                file.read(buffer, section_size);
+                SchemeFile.read(buffer, section_size);
 
                 SetConsoleTextAttribute(hConsole, 15);
                 for (uint32_t i = 0; i < section_size; ++i) {
-                    print_binary = buffer[i];
-                    std::cout << print_binary << ' ';
+                    print_byte = buffer[i];
+                    std::cout << print_byte << ' ';
+                    LogFile << print_byte << ' ';
                 }
             } else {
-                is_scheme_section = false;
+                is_main_section = false;
                 is_section_in_main = true;
                 main_section_size = section_size;
             }
@@ -144,15 +198,8 @@ int main() {
 
     }
 
-    file.close();
-
-    SetConsoleTextAttribute(hConsole, 12);
-    std::cout << '\n' << '\n';
-    std::cout << "file size: ";
-    SetConsoleTextAttribute(hConsole, 1);
-    std::cout << file_size;
-    SetConsoleTextAttribute(hConsole, 12);
-    std::cout << " bytes \n";
+    SchemeFile.close();
+    LogFile.close();
 
     return 0;
 }
