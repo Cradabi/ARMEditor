aaed15218164cdee7cffd19c39af6fe98963526c
4ekhov <seregarash@yandex.ru>
2024-02-18 21:59:39 +0300

    little update + add SchemeDataTypes structure

--- parser/SchemeFileParser.cpp
+++ parser/SchemeFileParser.cpp
@@ -1,5 +1,5 @@
-//#include "SchemeFileParser.h"
-//
+#include "SchemeFileParser.h"
+
 //struct SCHM_ITEM {
 //    std::string name;
 //    char type;
@@ -53,189 +53,30 @@
 //                                  {"PixelsPerInchY",                0}
 //
 //};
-//
-//void SchemeFileParser::ReadInfoInBlock(const uint32_t& block_size) {
-//    if (sections_stack.back().sect_name == "schm") {
-//        SchemeFile.read(buffer, block_size);
-//        WriteLog(schm_items[schm_obj_counter].name + ": ");
-//        if (schm_items[schm_obj_counter].type == 0 ||
-//            schm_items[schm_obj_counter].type == 2) { //чтение типа 0/2 (число)
-//            uint32_t block_value = 0;
-//            for (int32_t i = block_size - 1; i >= 0; --i) {
-//                block_value |= static_cast<uint8_t>(buffer[i]);
-//                if (i != 0) {
-//                    block_value <<= 8;
-//                }
-//            }
-//            if (schm_items[schm_obj_counter].type == 2) { //проверка размера (если = 0, отсутсвует вхождение)
-//                if (block_value == 0) {
-//                    schm_obj_counter += 1;
-//                }
-//            }
-//            WriteLog(block_value);
-//        } else { //чтение типа 1 (строка)
-//            for (uint32_t i = 0; i < block_size; ++i) {
-//                WriteLog(buffer[i]);
-//            }
-//        }
-//        WriteLog("\n");
-//        schm_obj_counter += 1;
-//    } else if (sections_stack.back().sect_name == "cach") {
-//        SchemeFile.read(buffer, block_size);
-//        WriteLog(cach_items[cach_obj_counter].name + ": ");
-//        if (cach_items[cach_obj_counter].type == 0 ||
-//            cach_items[cach_obj_counter].type == 2) { //чтение типа 0/2 (число)
-//            uint32_t block_value = 0;
-//            for (int32_t i = block_size - 1; i >= 0; --i) {
-//                block_value |= static_cast<uint8_t>(buffer[i]);
-//                if (i != 0) {
-//                    block_value <<= 8;
-//                }
-//            }
-//            if (cach_items[cach_obj_counter].type == 2) { //проверка размера (если = 0, отсутсвует вхождение)
-//                if (block_value == 0) {
-//                    cach_obj_counter += 1;
-//                }
-//            }
-//            WriteLog(block_value);
-//        } else { //чтение типа 1 (строка)
-//            for (uint32_t i = 0; i < block_size; ++i) {
-//                WriteLog(buffer[i]);
-//            }
-//        }
-//        WriteLog("\n");
-//        cach_obj_counter += 1;
-//    } else if (sections_stack.back().sect_name == "link") {
-//        SchemeFile.read(buffer, block_size);
-//        WriteLog(link_items[link_obj_counter].name + ": ");
-//        if (link_items[link_obj_counter].type == 0 ||
-//            link_items[link_obj_counter].type == 2) { //чтение типа 0/2 (число)
-//            uint32_t block_value = 0;
-//            for (int32_t i = block_size - 1; i >= 0; --i) {
-//                block_value |= static_cast<uint8_t>(buffer[i]);
-//                if (i != 0) {
-//                    block_value <<= 8;
-//                }
-//            }
-//            if (link_items[link_obj_counter].type == 2) { //проверка размера (если = 0, отсутсвует вхождение)
-//                if (block_value == 0) {
-//                    link_obj_counter += 1;
-//                }
-//            }
-//            WriteLog(block_value);
-//        } else { //чтение типа 1 (строка)
-//            for (uint32_t i = 0; i < block_size; ++i) {
-//                WriteLog(buffer[i]);
-//            }
-//        }
-//        WriteLog("\n");
-//        link_obj_counter += 1;
-//    } else if (sections_stack.back().sect_name == "sect") {
-//        SchemeFile.read(buffer, block_size);
-//        WriteLog(sect_items[sect_obj_counter].name + ": ");
-//        if (sect_items[sect_obj_counter].type == 0 ||
-//            sect_items[sect_obj_counter].type == 2) { //чтение типа 0/2 (число)
-//            uint32_t block_value = 0;
-//            for (int32_t i = block_size - 1; i >= 0; --i) {
-//                block_value |= static_cast<uint8_t>(buffer[i]);
-//                if (i != 0) {
-//                    block_value <<= 8;
-//                }
-//            }
-//            if (sect_items[sect_obj_counter].type == 2) { //проверка размера (если = 0, отсутсвует вхождение)
-//                if (block_value == 0) {
-//                    sect_obj_counter += 1;
-//                }
-//            }
-//            WriteLog(block_value);
-//        } else { //чтение типа 1 (строка)
-//            for (uint32_t i = 0; i < block_size; ++i) {
-//                WriteLog(buffer[i]);
-//            }
-//        }
-//        WriteLog("\n");
-//        sect_obj_counter += 1;
-//    } else if (sections_stack.back().sect_name == "objs") {
-//        SchemeFile.read(buffer, block_size);
-//        WriteLog(objs_items[objs_obj_counter].name + ": ");
-//        if (objs_items[objs_obj_counter].type == 0 ||
-//            objs_items[objs_obj_counter].type == 2) { //чтение типа 0/2 (число)
-//            uint32_t block_value = 0;
-//            for (int32_t i = block_size - 1; i >= 0; --i) {
-//                block_value |= static_cast<uint8_t>(buffer[i]);
-//                if (i != 0) {
-//                    block_value <<= 8;
-//                }
-//            }
-//            if (objs_items[objs_obj_counter].type == 2) { //проверка размера (если = 0, отсутсвует вхождение)
-//                if (block_value == 0) {
-//                    objs_obj_counter += 1;
-//                }
-//            }
-//            WriteLog(block_value);
-//        } else { //чтение типа 1 (строка)
-//            for (uint32_t i = 0; i < block_size; ++i) {
-//                WriteLog(buffer[i]);
-//            }
-//        }
-//        WriteLog("\n");
-//        objs_obj_counter += 1;
-//    } else if (sections_stack.back().sect_name ==
-//               "extd") { //TODO эта секция может вызывать ошибки чтения (нереализованные функции, но нигде не используются)
-//        SchemeFile.read(buffer, block_size);
-//        WriteLog(extd_items[extd_obj_counter].name + ": ");
-//        if (extd_items[extd_obj_counter].type == 0 ||
-//            extd_items[extd_obj_counter].type == 2) { //чтение типа 0/2 (число)
-//            uint32_t block_value = 0;
-//            for (int32_t i = block_size - 1; i >= 0; --i) {
-//                block_value |= static_cast<uint8_t>(buffer[i]);
-//                if (i != 0) {
-//                    block_value <<= 8;
-//                }
-//            }
-//            if (extd_items[extd_obj_counter].type == 2) { //проверка размера (если = 0, отсутсвует вхождение)
-//                if (block_value == 0) {
-//                    extd_obj_counter += 1;
-//                }
-//            }
-//            WriteLog(block_value);
-//        } else { //чтение типа 1 (строка)
-//            for (uint32_t i = 0; i < block_size; ++i) {
-//                WriteLog(buffer[i]);
-//            }
-//        }
-//        WriteLog("\n");
-//        extd_obj_counter += 1;
-//    } else if (sections_stack.back().sect_name == "sch2") {
-//        SchemeFile.read(buffer, block_size);
-//        WriteLog(sch2_items[sch2_obj_counter].name + ": ");
-//        if (sch2_items[sch2_obj_counter].type == 0 ||
-//            sch2_items[sch2_obj_counter].type == 2) { //чтение типа 0/2 (число)
-//            uint32_t block_value = 0;
-//            for (int32_t i = block_size - 1; i >= 0; --i) {
-//                block_value |= static_cast<uint8_t>(buffer[i]);
-//                if (i != 0) {
-//                    block_value <<= 8;
-//                }
-//            }
-//            if (sch2_items[sch2_obj_counter].type == 2) { //проверка размера (если = 0, отсутсвует вхождение)
-//                if (block_value == 0) {
-//                    sch2_obj_counter += 2;
-//                }
-//            }
-//            WriteLog(block_value);
-//        } else { //чтение типа 1 (строка)
-//            for (uint32_t i = 0; i < block_size; ++i) {
-//                WriteLog(buffer[i]);
-//            }
-//        }
-//        WriteLog("\n");
-//        sch2_obj_counter += 1;
-//    } else {
-//        SchemeFile.read(buffer, block_size);
-//        WriteLog(block_size, true, 15);
-//    }
-//
-//    ClearBuffer();
-//
-//}
+
+void SchemeFileParser::ParseBlockData(const uint32_t& block_size) {
+
+    TSchemeDataType data_types; // Экземпляр типов данных схемы
+
+    // TODO при работе раскомментировать
+
+    //    uint32_t bytes_counter = 0;
+    //    while (bytes_counter < block_size) {
+    //
+    //        // Информация в байте начинается с типа данных группы байтов (первый байт группы байтов в блоке)
+    //        ++bytes_counter;
+    //        SchemeFile.get(byte);
+    //        if (static_cast<uint8_t>(byte) == data_types.dtInteger) { // К примеру, если интеджер
+    //
+    //            // Далее идет кол-во параметров схемы/объекта этого типа данных, если = 0, значит ничего не будет
+    //            // если >2 (3 и более) значит что-то пошло не так, нужно смотреть
+    //            ++bytes_counter;
+    //            SchemeFile.get(byte);
+    //            uint8_t tmp_params_amount = static_cast<uint8_t>(byte);
+    //        }
+    //    }
+
+    UpdateBuffer(block_size);            // TODO при работе закомментировать
+    SchemeFile.read(buffer, block_size);    // TODO при работе закомментировать
+    ClearBuffer();
+}
--- parser/SchemeFileParser.h
+++ parser/SchemeFileParser.h
@@ -19,13 +19,13 @@ private:
 
     uint64_t file_size;           // Размер файла схемы
 
-    uint8_t section_flag = 0b00000000;      // Признак секции
-    uint8_t block_flag = 0b10000000;        // Признак блока
+    static constexpr uint8_t section_flag = 0b00000000;      // Признак секции
+    static constexpr uint8_t block_flag = 0b10000000;        // Признак блока
 
-    uint8_t size_6_bit = 0b11000000;        // Признак блока с длиной 6 бит "1 1 xxxxxx", xxxxxx - размер блока
-    uint8_t size_8_bit = 0b10000000;        // Признак блока с длиной 8 бит
-    uint8_t size_16_bit = 0b10000001;       // Признак блока с длиной 16 бит
-    uint8_t size_32_bit = 0b10000010;       // Признак блока с длиной 32 бит
+    static constexpr uint8_t size_6_bit = 0b11000000;        // Признак блока с длиной 6 бит "1 1 xxxxxx", xxxxxx - размер блока
+    static constexpr uint8_t size_8_bit = 0b10000000;        // Признак блока с длиной 8 бит
+    static constexpr uint8_t size_16_bit = 0b10000001;       // Признак блока с длиной 16 бит
+    static constexpr uint8_t size_32_bit = 0b10000010;       // Признак блока с длиной 32 бит
 
 #pragma pack(push, 1)
     // Структура секции
@@ -37,6 +37,30 @@ private:
 
         std::string sect_name;          // Имя секции (пустое, если есть номер)
         uint32_t sect_number{0};        // Номер секции (пустой, если есть имя)
+    };
+
+    // Структура, хранящяя типы данных используемые в схеме
+    struct TSchemeDataType {
+        static constexpr uint8_t dtUnknown = 0x00;   // Неизвестный тип данных, нужен для поиска ошибок
+
+        // Записи фиксированной длины
+        static constexpr uint8_t dtInteger = 0x01;       // rtInteger (Целое число)
+        static constexpr uint8_t dtFloat = 0x02;         // rtFloat (Вещественное число)
+        static constexpr uint8_t dtPoint = 0x03;         // rtPoint (Точка)
+        static constexpr uint8_t dtFont = 0x04;          // rtFont (Шрифт)
+        static constexpr uint8_t dtByte = 0x05;          // rtByte (Байт)
+        static constexpr uint8_t dtRectangle = 0x06;     // rtRect (Прямоугольник)
+
+        // Записи переменной длины
+        static constexpr uint8_t dtString = 0x0A;        // rtString (Строка)
+        static constexpr uint8_t dtList = 0x1E;          // rtList (Список)
+        static constexpr uint8_t dtObject = 0x64;        // rtObject (Объект)
+        static constexpr uint8_t dtRecord = 0x65;        // rtRecord (Запись)
+        static constexpr uint8_t dtElement = 0x66;       // rtElement (Элемент контейнера)
+        static constexpr uint8_t dtLibraryObject = 0x67; // (Библиотечный объект)
+        static constexpr uint8_t dtLibrary = 0x96;       // rtLibrary (Библиотека (без сигнатуры))
+        static constexpr uint8_t dtBitmap = 0xC8;        // rtBitmap (Bitmap graphic)
+        static constexpr uint8_t dtJPEGImage = 0xC9;     // rtJPEG (JPEG graphic)
     };
 #pragma pack(pop)
 
@@ -224,30 +248,28 @@ private:
         // По признаку блока определяем его размер
         if ((static_cast<uint8_t>(byte) & size_6_bit) == size_6_bit)
             tmp_block_size |= (static_cast<uint8_t>(byte) & 0b00111111);
-
-        else if (static_cast<uint8_t>(byte) == size_8_bit)
-            tmp_bytes_for_blocksize = 1;
-        else if (static_cast<uint8_t>(byte) == size_16_bit)
-            tmp_bytes_for_blocksize = 2;
-        else if (static_cast<uint8_t>(byte) == size_32_bit)
-            tmp_bytes_for_blocksize = 4;
-
-        if (tmp_bytes_for_blocksize != 0 || tmp_block_size != 0) {
+        else {
             // Если размер блока не уместился в 6 бит, берём его исходя из нужного признака
-            if (tmp_block_size == 0) {
-                tmp_block_size = GetSomeInt(tmp_block_size, tmp_bytes_for_blocksize);
+            switch (static_cast<uint8_t>(byte)) {
+                case size_8_bit:
+                    tmp_bytes_for_blocksize = 1;
+                    break;
+                case size_16_bit:
+                    tmp_bytes_for_blocksize = 2;
+                    break;
+                case size_32_bit:
+                    tmp_bytes_for_blocksize = 4;
+                    break;
             }
 
-            UpdateBuffer(tmp_block_size);
-            SchemeFile.read(buffer, tmp_block_size);
-            ClearBuffer();
-
-            // ReadInfoInBlock(tmp_block_size);
-
+            tmp_block_size = GetSomeInt(tmp_block_size, tmp_bytes_for_blocksize);
         }
+
+        ParseBlockData(tmp_block_size);
     }
 
-    // void ReadInfoInBlock(const uint32_t& block_size);
+    // Функция чтения информации из блока
+    void ParseBlockData(const uint32_t& block_size);
 
 public:
     // Главная функция парсера схемы
