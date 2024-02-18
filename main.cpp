4af4d6fab6fc949617ce379357b7756bbbee6ce2
w-o-p <sergeylit04@gmail.com>
2024-02-18 16:46:28 +0300

    Changed size

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     MainWindow MainWindow;
+    MainWindow.setFixedSize(1280,800);
     MainWindow.show();
 
     return app.exec();
