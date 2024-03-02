8b4eef99a049d683e1b3592991b31573b2d365d8
ShinpiMeiro <57195050+ShinpiMeiro@users.noreply.github.com>
2024-03-03 00:04:06 +0300

    parsing info for text

--- PARSERLIB/parser.cpp
+++ PARSERLIB/parser.cpp
@@ -16,7 +16,7 @@ int main() {
 
     SchemeFileParser NewParser;
 
-    NewParser.parse(L"..\\schemes_exp\\Мультищитовая3.схема", "..\\logs\\SchemeLogs.txt");
+    NewParser.parse(L"..\\schemes_exp\\Текст.схема", "..\\logs\\SchemeLogs.txt");
 
     return 0;
 }
