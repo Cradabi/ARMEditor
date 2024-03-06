fb2c86c7f08437ba1bb427556aef71e1ecd204c7
Daniel <75375225+Cradabi@users.noreply.github.com>
2024-03-06 17:59:38 +0300

    Starting LibraryObject parser

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -20,7 +20,7 @@ MyWidget::MyWidget() {
     Scheme::SchemeParams tmp_scheme_params;
 
     SchemeFileParser parser(tmp_scheme_params);
-    parser.parse("../PARSERLIB/schemes_exp/Полигон3.схема",
+    parser.parse("../PARSERLIB/schemes_exp/razultuvert2.схема",
                  "../PARSERLIB/logs/SchemeLogs.txt");
 
 
